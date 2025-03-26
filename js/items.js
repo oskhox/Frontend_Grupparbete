6417b13d84967a01c2927e5f1e55695ee358fd3d
Oskar Høxbroe <oskar.hoxbroe@gmail.com>
2025-03-26 21:16:22 +0100

    basic kort

---
+++ js/items.js
@@ -0,0 +1,33 @@
+function getItems() {
+        fetch('https://fakestoreapi.com/products')
+            .then(response => response.json())
+            .then(data => renderItems(data))
+
+    function renderItems(items) {
+        let output = "";
+
+        items.forEach(item => {
+            output += `
+            <div class="col-md-3">
+            <div class="card h-100">
+            <img src="${item.image}" class="card-img-top img-fluid p-4" alt="${item.title}">
+            <div class="card-body">
+            <h5 class="card-title">${item.title}</h5>
+            <!--Beskrivnings-knapp-->
+            <div class="collapse" id="description-${item.id}">
+            <p>${item.description}</p>
+            </div>
+            <button class="btn btn-primary mb-2 w-100" data-bs-toggle="collapse" data-bs-target="#description-${item.id}">
+            Produktbeskrivning </button>
+            <!--Pris-knapp-->
+            <a href="#" class="btn btn-primary w-100">${item.price} EUR</a>
+            </div>
+            </div>
+            </div>
+            `;
+        })
+        document.getElementById("items").innerHTML = output;
+    }
+}
+
+getItems();
