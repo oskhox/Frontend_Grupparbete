6750a71b95ff34e3653a074c9c1a22784b19c00d
Oskar Høxbroe <oskar.hoxbroe@gmail.com>
2025-03-27 18:13:27 +0100

    fixade footer

--- js/items.js
+++ js/items.js
@@ -8,22 +8,26 @@ function getItems() {
 
         items.forEach(item => {
             output += `
-            <div class="col-md-3">
-            <div class="card h-100">
-            <img src="${item.image}" class="card-img-top img-fluid p-4" alt="${item.title}">
-            <div class="card-body">
+    <div class="col-md-3">
+    <div class="card h-100">
+        <img src="${item.image}" class="card-img-top img-fluid p-4" alt="${item.title}">
+        <div class="card-body">
             <h5 class="card-title">${item.title}</h5>
             <!--Beskrivnings-knapp-->
             <div class="collapse" id="description-${item.id}">
-            <p>${item.description}</p>
+                <p>${item.description}</p>
             </div>
+        </div>
+        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
             <button class="btn btn-primary mb-2 w-100" data-bs-toggle="collapse" data-bs-target="#description-${item.id}">
-            Produktbeskrivning </button>
+                Produktbeskrivning
+            </button>
             <!--Pris-knapp-->
             <a href="#" class="btn btn-primary w-100">${item.price} EUR</a>
-            </div>
-            </div>
-            </div>
+        </div>
+    </div>
+    </div>
+
             `;
         })
         document.getElementById("items").innerHTML = output;
