f4698784014e4f87615bb8028b58ce1de4c0e37a
Oskar Høxbroe <oskar.hoxbroe@gmail.com>
2025-03-28 14:15:30 +0100

    korten fungerar

--- js/items.js
+++ js/items.js
@@ -1,33 +1,33 @@
 function getItems() {
-        fetch('https://fakestoreapi.com/products')
-            .then(response => response.json())
-            .then(data => renderItems(data))
+    fetch('https://fakestoreapi.com/products')
+        .then(response => response.json())
+        .then(data => renderItems(data))
 
     function renderItems(items) {
         let output = "";
 
         items.forEach(item => {
             output += `
-    <div class="col-md-3">
-    <div class="card h-100">
+        <div class="col-md-3">
+        <div class="card shadow-sm">
         <img src="${item.image}" class="card-img-top img-fluid p-4" alt="${item.title}">
         <div class="card-body">
-            <h5 class="card-title">${item.title}</h5>
-            <!--Beskrivnings-knapp-->
+               <div class="title-container">
+                <h5 class="card-title">${item.title}</h5> </div>
             <div class="collapse" id="description-${item.id}">
                 <p>${item.description}</p>
             </div>
         </div>
         <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-            <button class="btn btn-primary mb-2 w-100" data-bs-toggle="collapse" data-bs-target="#description-${item.id}">
-                Produktbeskrivning
-            </button>
-            <!--Pris-knapp-->
-            <a href="#" class="btn btn-primary w-100">${item.price} EUR</a>
+            <h6 class="price">${Math.ceil(item.price * 10)} kr</h6> 
+            <!--Beskrivnings-knapp-->
+            <button class="btn btn-light mb-2 w-100" data-bs-toggle="collapse" data-bs-target="#description-${item.id}">
+                Mer info </button>
+            <!--Beställnings-knapp-->
+            <a href="#" class="btn btn-light w-100" id="btn2">Beställ</a>
         </div>
-    </div>
-    </div>
-
+        </div>
+        </div>
             `;
         })
         document.getElementById("items").innerHTML = output;
