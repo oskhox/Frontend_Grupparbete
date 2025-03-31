0584696d304babd6172e6fe9e9268f7d3b71f7ef
petra <petrakoski@hotmail.com>
2025-03-31 09:22:18 +0200

    Updated form handling with validation

--- js/form.js
+++ js/form.js
@@ -7,44 +7,103 @@ function saveData(e) {
     const emailInput = document.getElementById("email").value
     const phoneNumberInput = document.getElementById("phonenumber").value
     const addressInput = document.getElementById("address").value
-    const zipcodeInput = document.getElementById("zipcode").value
+    const zipCodeInput = document.getElementById("zipcode").value
     const cityInput = document.getElementById("city").value
 
+    let confirmOrder = true;
 
-    if (nameInput.length <2 || nameInput.length > 50) {
-        alert("Felakting inmatning, försök igen")
+    if (!validateName(nameInput)) confirmOrder = false;
+    if (!validateEmail(emailInput)) confirmOrder = false;
+    if (!validatePhoneNumber(phoneNumberInput)) confirmOrder = false;
+    if (!validateAddress(addressInput)) confirmOrder = false;
+    if (!validateZipCode(zipCodeInput)) confirmOrder = false;
+    if (!validateCity(cityInput)) confirmOrder = false;
+
+    if (!confirmOrder) {
+        //alert("Vänligen fyll i alla fält.")
         return;
     }
 
-    if(!emailInput.includes('@') || emailInput.length > 50)
-    {
-        alert("Felaktig inmatning, försök igen")
-        return;
+    alert("Beställningen skickad")
+
+    localStorage.setItem("order", JSON.stringify({
+        name: nameInput,
+        email: emailInput,
+        phoneNumber: phoneNumberInput,
+        address: addressInput,
+        zipCode: zipCodeInput,
+        city: cityInput
+    }));
+
+    function validateName(nameInput) {
+        let validateNameInput = document.getElementById("inputValidationName")
+
+        if (nameInput.length < 2 || nameInput.length > 50) {
+            validateNameInput.style.display = "block";
+            return false;
+        } else {
+            validateNameInput.style.display = "none";
+            return true;
+        }
     }
 
-    if (!/^[\d\-()]{1,50}$/.test(phoneNumberInput))
-    {
-        alert("Felaktig inmatning")
-        return;
+    function validateEmail(emailInput) {
+        let validateEmailInput = document.getElementById("inputValidationEmail")
+
+        if (!emailInput.includes('@') || emailInput.length > 50) {
+            validateEmailInput.style.display = "block";
+            return false;
+        } else {
+            validateEmailInput.style.display = "none";
+            return true;
+
+        }
     }
 
-    if(addressInput.length < 2 || addressInput.length > 50) {
-        alert("Felaktig inmatning")
-        return;
+    function validatePhoneNumber(phoneNumberInput) {
+        let validatePhoneNumberInput = document.getElementById("inputValidationPhoneNumber")
+        if (!/^[\d\-()]{1,50}$/.test(phoneNumberInput)) {
+            validatePhoneNumberInput.style.display = "block";
+            return false;
+        } else {
+            validatePhoneNumberInput.style.display = "none";
+            return true;
+        }
     }
 
-    if(zipcodeInput.length !== 5 || isNaN(zipcodeInput)) {
-        alert("felaktig inmatning")
-        return;
+    function validateAddress(addressInput) {
+        let validateAddressInput = document.getElementById("inputValidationAddress")
+
+        if (addressInput.length < 2 || addressInput.length > 50) {
+            validateAddressInput.style.display = "block";
+            return false;
+        } else {
+            validateAddressInput.style.display = "none";
+            return true;
+        }
     }
 
-    if(cityInput.length < 2 || cityInput.length > 50) {
-        alert("felaktig inmatnig")
-        return;
+    function validateZipCode(zipCodeInput) {
+        let validateZipCodeInput = document.getElementById("inputValidationZipCode")
+
+        if (zipCodeInput.length !== 5 || isNaN(zipCodeInput)) {
+            validateZipCodeInput.style.display = "block";
+            return false;
+        } else {
+            validateZipCodeInput.style.display = "none";
+            return true;
+        }
     }
 
-    {
-        alert("Beställningen är skickad!")
+    function validateCity(cityInput) {
+        let validateCityInput = document.getElementById("inputValidationCity")
+        if (cityInput.length < 2 || cityInput.length > 50) {
+            validateCityInput.style.display = "block";
+            return false;
+        } else {
+            validateCityInput.style.display = "none";
+            return true;
+        }
     }
-
 }
+
