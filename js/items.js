6ea0856b499ec9fbfa0233b62173741b4eb24216
Oskar Høxbroe <oskar.hoxbroe@gmail.com>
2025-04-02 09:19:59 +0200

    fixade så även korten på framsidan har minimal skugga

--- js/items.js
+++ js/items.js
@@ -18,11 +18,11 @@ function getItems() {
         <div class="card shadow-sm">
         <img src="${item.image}" class="card-img-top p-4" alt="${item.title}">
         <div class="card-body p-3 p-md-4">
-               <div class="title-container">
-               <h5 class="card-title">${item.title}</h5> </div>
-            <div class="collapse" id="description-${item.id}">
+              <div class="title-container">
+               <h5 class="card-title">${item.title}</h5></div>
+                <div class="collapse" id="description-${item.id}">
                 <p>${item.description}</p>
-            </div>
+              </div>
         </div>
         <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
             <h6 class="price">${Math.ceil(item.price * 10)} kr</h6> 
@@ -54,7 +54,7 @@ function getFourItemsByCategory(category, containerId) {
         items.slice(0, 4).forEach(item => {
             output += `
                 <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
-                    <div class="card">
+                    <div class="card shadow-sm">
                         <img src="${item.image}" class="card-img-top p-4" alt="${item.title}">
                     </div>
                 </div>
